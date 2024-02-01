94e3583572d6761db16245fc2cdad85169176f37
David gomez <hdgomezv@gmail.com>
2024-02-01 23:12:59 +0100

    crear funciones de verificacion de minusculas y de verificacion de caracteres especiales.

--- README.md
+++ README.md
@@ -11,3 +11,18 @@
 - Área para mostrar el texto encriptado/desencriptado.-->
 
 30-01-24 Se realizan los TO DO pendientes, se elabora un JS provisional y se trabaja en el CSS.
+
+Requisitos:
+
+
+Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+Por ejemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+
+La página debe tener campos para
+inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
+El resultado debe ser mostrado en la pantalla.
+Extras:
+
+Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 function encriptar() {
   var mensajeEntrada = document.getElementById("caja-mensaje").value;
-  var mensajeEncriptado = encriptarMensaje(mensajeEntrada);
-  document.getElementById("caja-resultado").value = mensajeEncriptado;
+
+  if (validarCaracteresEspeciales(mensajeEntrada)) {
+    var mensajeVerificado = verificarTexto(mensajeEntrada);
+    var mensajeEncriptado = encriptarMensaje(mensajeVerificado);
+    document.getElementById("caja-resultado").value = mensajeEncriptado;
+  } else {
+    alert(
+      "La frase contiene caracteres especiales. Por favor, utiliza solo letras minúsculas y sin acentos."
+    );
+  }
 }
 
 function desencriptar() {
   var mensajeEncriptado = document.getElementById("caja-mensaje").value;
-  var mensajeDesencriptado = desencriptarMensaje(mensajeEncriptado);
-  document.getElementById("caja-resultado").value = mensajeDesencriptado;
+
+  if (validarCaracteresEspeciales(mensajeEncriptado)) {
+    var mensajeVerificado = verificarTexto(mensajeEncriptado);
+    var mensajeDesencriptado = desencriptarMensaje(mensajeVerificado);
+    document.getElementById("caja-resultado").value = mensajeDesencriptado;
+  } else {
+    "La frase contiene caracteres especiales. Por favor, utiliza solo letras minúsculas y sin acentos.";
+  }
+}
+
+
+function verificarTexto(mensaje) {
+  if (mensaje == mensaje.toLowerCase()) {
+    return mensaje;
+  } else {
+    alert(
+      "El texto debe  estar en letra minúscula... se procederá a cambiarlo"
+    );
+    return mensaje.toLowerCase();
+  }
+}
+
+function validarCaracteresEspeciales(mensaje) {
+  var caracteresEspeciales = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\\/`'"|=]/;
+
+  if (caracteresEspeciales.test(mensaje)) {
+    // Se encontraron caracteres especiales, la frase no es válida.
+    return false;
+  } else {
+    // No se encontraron caracteres especiales, la frase es válida.
+    return true;
+  }
 }
 
 function encriptarMensaje(mensaje) {
