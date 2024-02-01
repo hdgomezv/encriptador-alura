67b3990026a734f4491908e2a3fe95419723609c
David gomez <hdgomezv@gmail.com>
2024-02-01 22:28:16 +0100

    actualizacion de js y style

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
 function encriptar() {
-  var inputText = document.getElementById("mensaje-box").value;
-  var encryptedText = inputText
+  var mensajeEntrada = document.getElementById("caja-mensaje").value;
+  var mensajeEncriptado = encriptarMensaje(mensajeEntrada);
+  document.getElementById("caja-resultado").value = mensajeEncriptado;
+}
+
+function desencriptar() {
+  var mensajeEncriptado = document.getElementById("caja-mensaje").value;
+  var mensajeDesencriptado = desencriptarMensaje(mensajeEncriptado);
+  document.getElementById("caja-resultado").value = mensajeDesencriptado;
+}
+
+function encriptarMensaje(mensaje) {
+  return mensaje
     .replace(/a/g, "ai")
     .replace(/e/g, "enter")
     .replace(/i/g, "imes")
     .replace(/o/g, "ober")
     .replace(/u/g, "ufat");
-  document.getElementById("mensaje-resultado-box").value = encryptedText;
 }
 
-function desencriptar() {
-  var encryptedText = document.getElementById("mensaje-box").value;
-  var originalText = encryptedText
+function desencriptarMensaje(mensajeEncriptado) {
+  return mensajeEncriptado
     .replace(/ufat/g, "u")
     .replace(/ober/g, "o")
     .replace(/imes/g, "i")
     .replace(/enter/g, "e")
     .replace(/ai/g, "a");
-  document.getElementById("mensaje-resultado-box").value = originalText;
+}
+
+function actualizarResultado(contenido) {
+  var cajaResultado = document.getElementById("caja-resultado");
+  var contenidoInicial = document.getElementById("contenido-inicial");
+
+  cajaResultado.value = contenido;
+
+  // Oculta el contenido inicial cuando se muestra la respuesta
+  contenidoInicial.style.display = "none";
+  // Muestra el cuadro de resultado
+  cajaResultado.style.display = "block";
 }
